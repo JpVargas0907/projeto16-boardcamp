470a87ed2d7a967352b2b5823f1b1c25965fb98e
João Paulo <joaopaulo0907@gmail.com>
2023-02-11 14:18:49 -0300

    Rotas para entidade games finalizada, com middleware e validação dos dados com joi

---
+++ middleware/validateGame.js
@@ -0,0 +1,20 @@
+import { db } from '../database/postgres.js';
+import { gameSchema } from '../schemas/gameSchema.js';
+
+async function validateGame(req, res, next){
+    const validation = gameSchema.validate(req.body);
+    const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
+    const verifyGameName = await db.query(`SELECT * FROM games WHERE name = $1`, [name]);
+
+    if(validation.error){
+        return res.sendStatus(422);
+    } else if (name === null || stockTotal <= 0 || pricePerDay <= 0){
+        return res.sendStatus(400);
+    } else if (verifyGameName){
+        return res.sendStatus(409);
+    }
+
+    next();
+}
+
+export default validateGame;
--- routers/gamesRouters.js
+++ routers/gamesRouters.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { searchGames, registerGame } from '../controllers/gamesController.js';
+import validateGame from '../middleware/validateGame.js';
 
 const router = Router();
 
 router.get("/games", searchGames);
-router.post("/games", registerGame);
+router.post("/games", validateGame , registerGame);
 
 export default router;
