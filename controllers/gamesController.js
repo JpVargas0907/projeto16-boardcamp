159f5c0c29aa10fc1b1bd3f442f8e6862ebe1b09
João Paulo <joaopaulo0907@gmail.com>
2023-02-13 10:34:31 -0300

    alteração nas tabelas games

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -12,7 +12,7 @@ export async function searchGames(req, res){
 export async function registerGame(req, res){
     const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
     try {
-        await db.query(`INSERT INTO games (name, image, stockTotal, categoryId, pricePerDay) VALUES ($1, $2, $3, $4, $5)`, [name, image, stockTotal, categoryId, pricePerDay]);
+        await db.query(`INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ($1, $2, $3, $4, $5)`, [name, image, stockTotal, categoryId, pricePerDay]);
         return res.sendStatus(201);
     } catch (error) {
         return res.status(500).send(error.message);
