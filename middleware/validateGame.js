6bc787cbe3a62e22b0a982c702f520337edf814a
João Paulo <joaopaulo0907@gmail.com>
2023-02-13 15:44:16 -0300

    Retirando categoryId

--- middleware/validateGame.js
+++ middleware/validateGame.js
@@ -3,7 +3,7 @@ import { gameSchema } from '../schemas/gameSchema.js';
 
 async function validateGame(req, res, next){
     const validation = gameSchema.validate(req.body);
-    const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
+    const { name, image, stockTotal, pricePerDay } = req.body;
     const verifyGameNameExists = await db.query(`SELECT * FROM games WHERE name = $1`, [name]);
 
     if(validation.error){
