b97dd341e89af0edb63099666e5aaa9543717a0d
João Paulo <joaopaulo0907@gmail.com>
2023-02-13 10:11:38 -0300

    ajuste rotas games

--- schemas/gameSchema.js
+++ schemas/gameSchema.js
@@ -4,6 +4,5 @@ export const gameSchema = joi.object({
     name: joi.string().required(),
     image: joi.string().required(),
     stockTotal: joi.number().required(),
-    categoryId: joi.number().required(),
     pricePerDay: joi.number().required(),
 })
