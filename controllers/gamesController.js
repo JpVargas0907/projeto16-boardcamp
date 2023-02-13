dd4dd2ef23607a37f906af2539fe8f3457e48ac4
João Paulo <joaopaulo0907@gmail.com>
2023-02-13 10:16:59 -0300

    Voltando pra versão anterior da rota

--- controllers/gamesController.js
+++ controllers/gamesController.js
@@ -10,9 +10,9 @@ export async function searchGames(req, res){
 }
 
 export async function registerGame(req, res){
-    const { name, image, stockTotal, pricePerDay } = req.body;
+    const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
     try {
-        await db.query(`INSERT INTO games (name, image, stockTotal, pricePerDay) VALUES ($1, $2, $3, $4)`, [name, image, stockTotal, pricePerDay]);
+        await db.query(`INSERT INTO games (name, image, stockTotal, categoryId, pricePerDay) VALUES ($1, $2, $3, $4, $5)`, [name, image, stockTotal, categoryId, pricePerDay]);
         return res.sendStatus(201);
     } catch (error) {
         return res.status(500).send(error.message);
--- schemas/gameSchema.js
+++ schemas/gameSchema.js
@@ -4,5 +4,6 @@ export const gameSchema = joi.object({
     name: joi.string().required(),
     image: joi.string().required(),
     stockTotal: joi.number().required(),
+    categoryId: joi.number().required(),
     pricePerDay: joi.number().required(),
 })
